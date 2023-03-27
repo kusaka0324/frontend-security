96761c8714365defe2bbafc48c84393e5020a5bf
kusaka0324 <haruto.k.4230@gmail.com>
2023-03-27 19:35:07 +0900

    move : ディレクトリ変更

---
+++ src/routes/api.js
@@ -0,0 +1,30 @@
+//ルーティング用のオブジェクトを生成
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.setHeader("X-Timestamp", Date.now());
+  let message = req.query.message;
+  const lang = req.headers["x-lang"];
+
+  if(message === "") {
+    res.status(400);
+    if(lang === "en") {
+      message = "message is empty.";
+    } else {
+      message = "messageの値が空です";      
+    }
+  }
+  res.send({ message });
+});
+
+router.use(express.json());
+router.post("/", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.end();
+});
+
+//他ファイルから読み込めるようにエクスポート
+module.exports = router;
+
